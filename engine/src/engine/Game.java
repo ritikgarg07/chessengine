72722025c636ecdc16e301161d8e5f52fe255733
harsh815 <harshyadav0409@gmail.com>
2020-02-02 17:01:29 +0530

    associated testing change in Game

--- engine/src/engine/Game.java
+++ engine/src/engine/Game.java
@@ -3,7 +3,7 @@ package engine;
 public class Game {
 	
 	Player user = new Player();
-	Player ai= new Player();
+	Player ai = new Player();
 	
 	public void setUserColour(Colour colour) {
 		switch(colour) {
@@ -20,6 +20,15 @@ public class Game {
 		Game game =  new Game();
 		game.setUserColour(Colour.WHITE);
 		Board currentBoard = new Board(game.user, game.ai);
+		currentBoard.displayBoard();
+		Move move= Move.newMove(99,42);
+		if(move!=null) {
+			currentBoard.updateBoard(move);
+		}
+		else {
+			System.out.print("\ninvalid!!\n");
+		}
+		currentBoard.updateBoard(Move.newMove(12, 28));
 		currentBoard.displayBoard();
 	}
 }
