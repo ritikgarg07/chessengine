be8580b99af466a54ba3e77f1cf71f45d2aa3db8
harsh815 <harshyadav0409@gmail.com>
2020-02-02 12:19:44 +0530

    Implemented player with colour field(enum)

--- engine/src/engine/Player.java
+++ engine/src/engine/Player.java
@@ -3,5 +3,14 @@ enum Colour{
 	WHITE,BLACK;
 }
 public class Player {
-	Colour colour=Colour.BLACK;
+	
+	private Colour colour;
+	
+	public void setColour(Colour colour) {
+		this.colour = colour;
+	}
+	
+	public Colour getColour() {
+		return this.colour;
+	}
 }
