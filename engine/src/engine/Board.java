d77723c6c4b04dfd93098f17916ac47a5176227f
harsh815 <harshyadav0409@gmail.com>
2020-02-02 13:24:36 +0530

    Fixed mismatch between display board view and actual storage indexing: previously: user pieces stored in row 7,8 and displayed in 0,1 now: user pieces stored in row 0,1 and displayed in 0,1.

--- engine/src/engine/Board.java
+++ engine/src/engine/Board.java
@@ -13,41 +13,39 @@ public class Board {
 		this.ai = ai;
 		
 		for(int i=8;i<16;i++) {
-			board[i] = new Piece(Type.PAWN, i, ai);
-			board[i+40] = new Piece(Type.PAWN, i+40, user);
+			board[i] = new Piece(Type.PAWN, i, user);
+			board[i+40] = new Piece(Type.PAWN, i+40, ai);
 		}
-		board[0] = new Piece(Type.ROOK, 0, ai);
-		board[1] = new Piece(Type.KNIGHT, 1, ai);
-		board[2] = new Piece(Type.BISHOP, 2, ai);
-		board[3] = new Piece(Type.QUEEN, 3, ai);
-		board[4] = new Piece(Type.KING, 4, ai);
-		board[5] = new Piece(Type.BISHOP, 2, ai);
-		board[6] = new Piece(Type.KNIGHT, 1, ai);
-		board[7] = new Piece(Type.ROOK, 0, ai);
-		board[0+56] = new Piece(Type.ROOK, 0+56, user);
-		board[1+56] = new Piece(Type.KNIGHT, 1+56, user);
-		board[2+56] = new Piece(Type.BISHOP, 2+56, user);
-		board[3+56] = new Piece(Type.QUEEN, 3+56, user);
-		board[4+56] = new Piece(Type.KING, 4+56, user);
-		board[5+56] = new Piece(Type.BISHOP, 2+56, user);
-		board[6+56] = new Piece(Type.KNIGHT, 1+56, user);
-		board[7+56] = new Piece(Type.ROOK, 0+56, user);
+		board[0] = new Piece(Type.ROOK, 0, user);
+		board[1] = new Piece(Type.KNIGHT, 1, user);
+		board[2] = new Piece(Type.BISHOP, 2, user);
+		board[3] = new Piece(Type.QUEEN, 3, user);
+		board[4] = new Piece(Type.KING, 4, user);
+		board[5] = new Piece(Type.BISHOP, 2, user);
+		board[6] = new Piece(Type.KNIGHT, 1, user);
+		board[7] = new Piece(Type.ROOK, 0, user);
+		board[0+56] = new Piece(Type.ROOK, 0+56, ai);
+		board[1+56] = new Piece(Type.KNIGHT, 1+56, ai);
+		board[2+56] = new Piece(Type.BISHOP, 2+56, ai);
+		board[3+56] = new Piece(Type.QUEEN, 3+56, ai);
+		board[4+56] = new Piece(Type.KING, 4+56, ai);
+		board[5+56] = new Piece(Type.BISHOP, 2+56, ai);
+		board[6+56] = new Piece(Type.KNIGHT, 1+56, ai);
+		board[7+56] = new Piece(Type.ROOK, 0+56, ai);
 	}
 	
 	public void displayBoard(){
-		for(int i=0;i<64;i++) {
-			if(i%8==0) {
-				System.out.print(8-i/8+" ");
+		for(int row=7;row>=0;row--) {
+			System.out.print(row+1+" ");
+			for(int col=0;col<8;col++) {
+				if(board[row*8+col]!=null) {
+					System.out.print(board[row*8+col].getType()+" ");
+				}
+				else {
+					System.out.print("- ");
+				}
 			}
-			if(board[i]!=null) {
-				System.out.print(board[i].getType()+" ");
-			}
-			else {
-				System.out.print("- ");
-			}
-			if(i%8==7) {
-				System.out.println();
-			}
+			System.out.println();
 		}
 		System.out.print(" ");
 		for(int i=0;i<8;i++) {
