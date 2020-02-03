47868693ab0e7d81df893fcda116442591ecef4a
ritikgarg07 <ritikgarg07@gmail.com>
2020-02-03 19:54:30 +0530

    updated game to have 2 users instead of an ai; sets user1 to white

--- engine/src/engine/Game.java
+++ engine/src/engine/Game.java
@@ -1,25 +1,33 @@
 package engine;
 
+// import engine.Colour;
+
+/*	Wrapper class for each game instance
+*/
+
+// currently instantiates two users
+
 public class Game {
 	
-	Player user = new Player();
-	Player ai = new Player();
-	
+	Player user1, user2;
+
 	public void setUserColour(Colour colour) {
 		switch(colour) {
-			case WHITE: user.setColour(Colour.WHITE);
-						ai.setColour(Colour.BLACK);
+			case WHITE: user1 = new User(Colour.WHITE);
+						user2 = new User(Colour.BLACK);
 						break;
-			case BLACK: user.setColour(Colour.BLACK);
-						ai.setColour(Colour.WHITE);
+			case BLACK: user1 = new User(Colour.BLACK);
+						user2 = new User(Colour.WHITE);
 						break;			
 		}
 	}
 	
 	public static void main(String args[]) {
-		Game game =  new Game();
+		Game game = new Game();
+
+		// would ideally set User colour, for now it sets user1 colour;
 		game.setUserColour(Colour.WHITE);
-		Board currentBoard = new Board(game.user, game.ai);
+		Board currentBoard = new Board(game.user1, game.user2);
 		currentBoard.displayBoard();
 		Move move= Move.newMove(99,42);
 		if(move!=null) {
