9ea103a9bb4a510147bd8cabd482e1e7ca36fc9f
ritikgarg07 <ritikgarg07@gmail.com>
2020-02-03 18:32:01 +0530

    made board class independent of need to have an ai, now has two variables of player class

--- engine/src/engine/Board.java
+++ engine/src/engine/Board.java
@@ -1,37 +1,37 @@
-package engine;
+package engine.src.engine;
 
 public class Board {
 	Piece board [] = new Piece[64];
-	Player user,ai;
+	Player user1, user2;
 	/*
 	 * Board(FEN){
 	 * }
 	 */
-	Board(Player user, Player ai){
+	Board(Player user1, Player user2){
 		
-		this.user = user;
-		this.ai = ai;
+		this.user1 = user1;
+		this.user2 = user2;
 		
 		for(int i=8;i<16;i++) {
-			board[i] = new Piece(Type.PAWN, i, user);
-			board[i+40] = new Piece(Type.PAWN, i+40, ai);
+			board[i] = new Piece(Type.PAWN, i, user1);
+			board[i+40] = new Piece(Type.PAWN, i+40, user2);
 		}
-		board[0] = new Piece(Type.ROOK, 0, user);
-		board[1] = new Piece(Type.KNIGHT, 1, user);
-		board[2] = new Piece(Type.BISHOP, 2, user);
-		board[3] = new Piece(Type.QUEEN, 3, user);
-		board[4] = new Piece(Type.KING, 4, user);
-		board[5] = new Piece(Type.BISHOP, 2, user);
-		board[6] = new Piece(Type.KNIGHT, 1, user);
-		board[7] = new Piece(Type.ROOK, 0, user);
-		board[0+56] = new Piece(Type.ROOK, 0+56, ai);
-		board[1+56] = new Piece(Type.KNIGHT, 1+56, ai);
-		board[2+56] = new Piece(Type.BISHOP, 2+56, ai);
-		board[3+56] = new Piece(Type.QUEEN, 3+56, ai);
-		board[4+56] = new Piece(Type.KING, 4+56, ai);
-		board[5+56] = new Piece(Type.BISHOP, 2+56, ai);
-		board[6+56] = new Piece(Type.KNIGHT, 1+56, ai);
-		board[7+56] = new Piece(Type.ROOK, 0+56, ai);
+		board[0] = new Piece(Type.ROOK, 0, user1);
+		board[1] = new Piece(Type.KNIGHT, 1, user1);
+		board[2] = new Piece(Type.BISHOP, 2, user1);
+		board[3] = new Piece(Type.QUEEN, 3, user1);
+		board[4] = new Piece(Type.KING, 4, user1);
+		board[5] = new Piece(Type.BISHOP, 2, user1);
+		board[6] = new Piece(Type.KNIGHT, 1, user1);
+		board[7] = new Piece(Type.ROOK, 0, user1);
+		board[0+56] = new Piece(Type.ROOK, 0+56, user2);
+		board[1+56] = new Piece(Type.KNIGHT, 1+56, user2);
+		board[2+56] = new Piece(Type.BISHOP, 2+56, user2);
+		board[3+56] = new Piece(Type.QUEEN, 3+56, user2);
+		board[4+56] = new Piece(Type.KING, 4+56, user2);
+		board[5+56] = new Piece(Type.BISHOP, 2+56, user2);
+		board[6+56] = new Piece(Type.KNIGHT, 1+56, user2);
+		board[7+56] = new Piece(Type.ROOK, 0+56, user2);
 	}
 	
 	public void displayBoard(){
